65cd48b2b2b13e5fecde41bc66160da7df7ffa97
EternalNewbiee <secuya.francis.michael@gmail.com>
2022-12-21 11:40:12 +0800

    MILESTONE D: Save Person objects to the list

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +28,65 @@ public class App extends JFrame{
     public App() {
         persons = new ArrayList<>();
         // TODO add implementations for all milestones here
+        rbCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfMonths.setEditable(false);
+                tfSalary.setEditable(false);
+            }
+        });
+        rbClerk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfMonths.setEditable(true);
+                tfSalary.setEditable(true);
+            }
+        });
+        rbManager.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfMonths.setEditable(true);
+                tfSalary.setEditable(true);
+            }
+        });
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               if (rbCustomer.isSelected()){
+                   String name = tfName.getText();
+                   int age = Integer.parseInt(tfAge.getText());
+                   Person p = new Person(name,age);
+                   taPersons.append("Customer - " + name + " (" + age + ")\n");
+                   tfName.setText("");
+                   tfAge.setText("");
+               }
+               if (rbClerk.isSelected()){
+                   String name = tfName.getText();
+                   int age = Integer.parseInt(tfAge.getText());
+                   int month_worked = Integer.parseInt(tfMonths.getText());
+                   double salary = Double.parseDouble(tfSalary.getText());
+                   Clerk c = new Clerk(name,age,month_worked,salary);
+                   taPersons.append("Clerk - " + name + " (" + age + ")\n");
+                   tfName.setText("");
+                   tfAge.setText("");
+                   tfSalary.setText("");
+                   tfMonths.setText("");
+               }
+                if (rbManager.isSelected()){
+                    String name = tfName.getText();
+                    int age = Integer.parseInt(tfAge.getText());
+                    int month_worked = Integer.parseInt(tfMonths.getText());
+                    double salary = Double.parseDouble(tfSalary.getText());
+                    Manager m = new Manager(name,age,month_worked,salary);
+                    taPersons.append("Manager - " + name + " (" + age + ") \n");
+                    tfName.setText("");
+                    tfAge.setText("");
+                    tfSalary.setText("");
+                    tfMonths.setText("");
+                }
+
+            }
+        });
     }
 
     public static void main(String[] args) {
